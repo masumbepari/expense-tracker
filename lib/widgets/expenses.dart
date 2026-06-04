96489322adb207b82f915859b20649a4e1704bbf
masumbepari <masum.fb.2025@gmail.com>
2026-06-04 16:41:40 +0600

    added expense item widget

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/widgets/expense_list/expenses_list.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Bandarban Travel",
+      amount: 10000,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: "Google Cloud",
+      amount: 300.00,
+      date: DateTime.now(),
+      category: Category.supscription,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("The Chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
